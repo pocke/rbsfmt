ea906526a9f15a87153c4086e1c3e70318c206b3
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2019-12-24 22:24:35 +0900

    WIP

--- lib/rbsfmt/runner.rb
+++ lib/rbsfmt/runner.rb
@@ -5,33 +5,67 @@ module Rbsfmt
     def initialize(source, out: $stdout)
       @original_source = source
       @out = out
-      @indent = 0
     end
 
     def run
+      @tokens = []
+
       trees = Ruby::Signature::Parser.parse_signature(@original_source)
-      trees.map do |tree|
-        format_decl tree
-      end.join("\n\n")
+      trees.each do |tree|
+        format tree
+      end
+      @tokens << [:newline] # EOF
+
+      @out.write join_tokens @tokens
     end
 
-    private def format_decl(node)
+    private def format(node)
       case node
       when Ruby::Signature::AST::Declarations::Class
-        name = node.name.name
-        @out.puts "class #{name}"
-        with_indent(INDENT_WIDTH) do
+        @tokens << [:raw, 'class'] << [:space_or_newline] << [:raw, node.name.name.to_s] << [:indent, INDENT_WIDTH]
+        node.members.each do |member|
+          format member
         end
-        @out.puts "end"
+        @tokens << [:dedent, INDENT_WIDTH] << [:raw, 'end']
+      when Ruby::Signature::AST::Members::MethodDefinition
+        @tokens << [:raw, 'def'] << [:space_or_newline] << [:raw, node.name.to_s] << [:nothing_or_newline] << [:raw, ':'] << [:space_or_newline] << [:raw, '(']
+        # TODO: args
+        @tokens << [:raw, ")"] << [:space_or_newline] << [:raw, '->'] << [:space_or_newline] << [:raw, 'void'] << [:newline]
       else
         raise "Unknown node: #{node.class}"
       end
     end
 
-    private def with_indent(n, &block)
-      @indent += n
-      block.call
-      @indent -= n
+    private def join_tokens(tokens)
+      res = +""
+      indent = 0
+      do_indent = false
+
+      tokens.each do |tok|
+        case tok.first
+        when :raw
+          res << (" " * indent) if do_indent
+          do_indent = false
+          res << tok[1]
+        when :newline
+          res << "\n"
+          do_indent = true
+        when :space_or_newline
+          res << ' '
+        when :nothing_or_newline
+          # TODO
+        when :indent
+          indent += tok[1]
+          res << "\n"
+          do_indent = true
+        when :dedent
+          indent -= tok[1]
+        else
+          raise "unknown token: #{tok}"
+        end
+      end
+
+      res
     end
   end
 end
--- test/smoke_test.rb
+++ test/smoke_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class SmokeTest < Minitest::Test
   def test_smoke
-    Pathname.glob('smoke/*.rbs').each do |path|
+    Pathname.glob('smoke/*.rbs').sort.each do |path|
+      puts path
       out = StringIO.new
       content = path.read
       Rbsfmt::Runner.new(content, out: out).run
