08517fdd6b82a6ea436d2b89a057a8e90b08f583
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2019-12-26 22:30:57 +0900

    Simple comment support

--- lib/rbsfmt/runner.rb
+++ lib/rbsfmt/runner.rb
@@ -10,7 +10,7 @@ module Rbsfmt
     def run
       @tokens = []
 
-      trees = Ruby::Signature::Parser.parse_signature(@original_source)
+      trees = parse
       trees.each do |tree|
         format tree
       end
@@ -20,19 +20,21 @@ module Rbsfmt
     end
 
     private def format(node)
+      preserve_comments node
+
       case node
       when Ruby::Signature::AST::Declarations::Class
-        @tokens << raw('class') << [:space_or_newline] << raw(node.name.name.to_s) << indent(INDENT_WIDTH) << [:newline]
+        @tokens << raw('class') << [:space] << raw(node.name.name.to_s) << indent(INDENT_WIDTH) << [:newline]
         node.members.each do |member|
           format member
         end
         @tokens << [:dedent, INDENT_WIDTH] << raw('end')
       when Ruby::Signature::AST::Members::MethodDefinition
-        @tokens << raw('def') << [:space_or_newline] << raw(node.name.to_s) << [:nothing_or_newline] << raw(':') << [:space_or_newline]
+        @tokens << raw('def') << [:space] << raw(node.name.to_s) << raw(':') << [:space]
         @tokens << indent(4 + node.name.to_s.size)
         node.types.each.with_index do |type, idx|
           format type
-          @tokens << [:newline] << raw("|") << [:space_or_newline] unless idx == node.types.size - 1
+          @tokens << [:newline] << raw("|") << [:space] unless idx == node.types.size - 1
         end
         @tokens << [:dedent, 4 + node.name.to_s.size]
         @tokens << [:newline]
@@ -44,13 +46,13 @@ module Rbsfmt
         node.required_positionals.each.with_index do |arg, idx|
           format arg
           # FIXME: trailing comma with multiline
-          @tokens << raw(',') << [:space_or_newline]  unless idx == node.required_positionals.size - 1
+          @tokens << raw(',') << [:space]  unless idx == node.required_positionals.size - 1
         end
-        @tokens << raw(")") << [:space_or_newline] << raw('->') << [:space_or_newline]
+        @tokens << raw(")") << [:space] << raw('->') << [:space]
         format node.return_type
       when Ruby::Signature::Types::Function::Param
         format node.type
-        @tokens << [:space_or_newline] << raw(node.name.to_s) if node.name
+        @tokens << [:space] << raw(node.name.to_s) if node.name
       when Ruby::Signature::Types::Bases::Base # any, void, etc.
         @tokens << raw(node.to_s)
       when Ruby::Signature::Types::ClassInstance
@@ -58,10 +60,29 @@ module Rbsfmt
       when Ruby::Signature::Types::Union
         node.types.each.with_index do |type, idx|
           format type
-          @tokens << [:space_or_newline] << raw("|") << [:space_or_newline] unless idx == node.types.size - 1
+          @tokens << [:space] << raw("|") << [:space] unless idx == node.types.size - 1
         end
       else
         raise "Unknown node: #{node.class}"
+      end
+    end
+
+    private def preserve_comments(node)
+      return unless node.respond_to? :location
+
+      while !@comments.empty? && @comments.first[0] < node.location.start_line
+        comment = @comments.shift[1]
+        comment.string.each_line do |line|
+          @tokens << [:raw, "# #{line.chomp}"] << [:newline]
+        end
+      end
+    end
+
+    private def parse
+      buffer = Ruby::Signature::Buffer.new(name: nil, content: @original_source)
+      parser = Ruby::Signature::Parser.new(:SIGNATURE, buffer: buffer, eof_re: nil)
+      parser.do_parse.tap do
+        @comments = parser.instance_variable_get(:@comments).to_a
       end
     end
 
@@ -79,10 +100,8 @@ module Rbsfmt
         when :newline
           res << "\n"
           do_indent = true
-        when :space_or_newline
+        when :space
           res << ' '
-        when :nothing_or_newline
-          # TODO
         when :indent
           indent += tok[1]
         when :dedent
